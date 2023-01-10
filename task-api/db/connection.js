62818d39198f0af38c665cdaed1cad616eeaa1e7
owais1947 <54672448+owais1947@users.noreply.github.com>
2023-01-10 12:22:09 +0530

    Update connection.js

--- task-api/db/connection.js
+++ task-api/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString = ' mongodb+srv://nismoowais1:Owais@123@nodejs.jb94vd5.mongodb.net/03-task-manager?retryWrites=true&w=majority'
+const connectionString = ' mongodb+srv://example:example.jb94vd5.mongodb.net/03-task-manager?retryWrites=true&w=majority'
 
 const connectDB = (url)=>{
    return mongoose.connect(url, 
